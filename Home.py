d524d7fb0a2a16a4386d5fabe5865a370499aabf
PriyeshDave <priyeshdave90@gmail.com>
2024-09-20 11:19:00 +0530

    API KEY ACCESS CHANGES

--- Home.py
+++ Home.py
@@ -36,7 +36,7 @@ warnings.filterwarnings("ignore", message="Lang Chain Version Update Warning.. "
 ############ New - Imports ##################
 
 print('############ New - Run ##################')
-
+ 
 # if authentication_status:
 
 st.set_page_config(page_title="Chat Doc Application - GitHub Copilot", page_icon="assets/images/favicon.png", layout="wide", initial_sidebar_state='collapsed')
@@ -105,11 +105,12 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-k1 = 'sk-IM4Ae4gJyGA3bMXMCFAQm2CVzermmzfd_5QDk'
-k2 = '-VAgWT3BlbkFJuSVlSjts3iB6GpyPFleJad48j3f'
-k3 = '_RGubRWLfEKkk8A'
-GPT_SECRETS = k1 + k2 + k3
+# k1 = 'sk-IM4Ae4gJyGA3bMXMCFAQm2CVzermmzfd_5QDk'
+# k2 = '-VAgWT3BlbkFJuSVlSjts3iB6GpyPFleJad48j3f'
+# k3 = '_RGubRWLfEKkk8A'
+# GPT_SECRETS = k1 + k2 + k3
 
+GPT_SECRETS = st.secrets["api"]["OPENAI_API_KEY"]
 #GPT_SECRETS = st.secrets["gpt_secret"]
 #GPT_SECRETS = os.getenv('GPT_SECRET')
 open_ai_gpt3.openai.api_key = GPT_SECRETS
